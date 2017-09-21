ef3bfa56e8b5030f809be1f117f98c668c513e46
lordramez <lordramez@LORDRAMEZ-PC>
2017-09-21 10:29:57 +0300

    Add project files.

---
+++ my_first/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello my name is rafat");
+            Console.ReadKey();
+        }
+    }
+}
